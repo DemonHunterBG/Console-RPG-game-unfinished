0de4e5d9736cedbe7a39f037355a35355550ea2c
Slavi <slavi.andonovvvv@gmail.com>
2021-04-15 02:51:34 +0300

    Ti si lud nomer 2

--- Program.cs
+++ Program.cs
@@ -803,7 +803,7 @@ namespace RPG_Game_2
             Console.ReadLine();
             Environment.Exit(0);
         }
-        static void BadEnd(Hero hero) //BADEND
+        static void BadEnd(Hero hero) //BADEND - second try
         {
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Red;
