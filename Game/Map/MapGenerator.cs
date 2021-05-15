4195ec07f7093687e61622d422f3a21e64297593
Slavi <slavi.andonovvvv@gmail.com>
2021-05-15 12:56:54 +0300

    Made blocking of start and ending rarer. Ending is now visible.

--- Game/Map/MapGenerator.cs
+++ Game/Map/MapGenerator.cs
@@ -9,6 +9,7 @@ namespace RPG_Game_2
     {
         private static char[] characters = { '-', '-', '-', '-', 'M', 'E', 'E', 'S' };
         private static char[] charactersnoShop = { '-', '-', '-', '-', '-', 'M', 'E', 'E' };
+        private static char[] charactersnoShopAndMountains = { '-', '-', '-', '-', '-', '-', 'E', 'E' };
 
         public static void MapGenController()
         {
@@ -29,8 +30,8 @@ namespace RPG_Game_2
                     break;
             }
 
-            int ROWS = random.Next(5, 8);
-            int COLS = random.Next(5, 8);
+            int ROWS = random.Next(6, 9);
+            int COLS = random.Next(6, 9);
 
             char[,] map = new char[ROWS, COLS];
             int numberofcharacters = 0;
@@ -59,10 +60,52 @@ namespace RPG_Game_2
                         map[row, col] = charactersnoShop[random.Next(0, charactersnoShop.Count())];
                 }
             }
+            MapStartAndEndAntiBlock(random, map);
+
             Map.map = map;
             MapVisibilityGen(ROWS, COLS);
 
         }
+
+        private static void MapStartAndEndAntiBlock(Random random, char[,] map)
+        {
+            int spawnAntiBlock = random.Next(1, 3);
+            int endAntiBlock = random.Next(1, 3);
+            switch (spawnAntiBlock)
+            {
+                case 1:
+                    if (map[1, 0] == 'M')
+                    {
+                        map[1, 0] = charactersnoShopAndMountains[random.Next(0, charactersnoShop.Count())];
+                    }
+                    break;
+                case 2:
+                    if (map[0, 1] == 'M')
+                    {
+                        map[0, 1] = charactersnoShopAndMountains[random.Next(0, charactersnoShop.Count())];
+                    }
+                    break;
+            }
+            map[1, 1] = charactersnoShopAndMountains[random.Next(0, charactersnoShop.Count())];
+
+            switch (endAntiBlock)
+            {
+                case 1:
+                    if (map[map.GetLength(0) - 2, map.GetLength(1) - 1] == 'M')
+                    {
+                        map[map.GetLength(0) - 2, map.GetLength(1) - 1] = charactersnoShopAndMountains[random.Next(0, charactersnoShop.Count())];
+                    }
+                    break;
+                case 2:
+                    if (map[map.GetLength(0) - 1, map.GetLength(1) - 2] == 'M')
+                    {
+                        map[map.GetLength(0) - 1, map.GetLength(1) - 2] = charactersnoShopAndMountains[random.Next(0, charactersnoShop.Count())];
+                    }
+                    break;
+            }
+            map[map.GetLength(0) - 2, map.GetLength(1) - 2] = charactersnoShopAndMountains[random.Next(0, charactersnoShop.Count())];
+        }
+
         private static void MapVisibilityGen(int ROWS, int COLS)
         {
             int[,] map = new int[ROWS, COLS];
@@ -73,6 +116,7 @@ namespace RPG_Game_2
                     map[row, col] = 0;
                 }
             }
+            map[ROWS - 1, COLS - 1] = 1;
             Map.mapVisibility = map;
         }
     }
